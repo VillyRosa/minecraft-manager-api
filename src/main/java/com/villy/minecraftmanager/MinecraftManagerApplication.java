ec8779ef11201ca89fe642a3f7f30a5bb640e0cc
Villy Rosa <villyrosa69@gmail.com>
2026-07-05 21:50:04 -0300

    chore: initial commit

---
+++ src/main/java/com/villy/minecraftmanager/MinecraftManagerApplication.java
@@ -0,0 +1,13 @@
+package com.villy.minecraftmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftManagerApplication.class, args);
+	}
+
+}
