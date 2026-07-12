27a853e85b064efd26796706305d734eee5d1017
Villy Rosa <villyrosa69@gmail.com>
2026-07-11 21:47:39 -0300

    feat: add minecraft id mapping to world enums

--- src/main/java/com/villy/minecraftmanager/enums/Difficulty.java
+++ src/main/java/com/villy/minecraftmanager/enums/Difficulty.java
@@ -1,8 +1,26 @@
 package com.villy.minecraftmanager.enums;
 
 public enum Difficulty {
-    PEACEFUL,
-    EASY,
-    NORMAL,
-    HARD
+    PEACEFUL(0),
+    EASY(1),
+    NORMAL(2),
+    HARD(3);
+
+    private final int id;
+
+    Difficulty(int id) {
+        this.id = id;
+    }
+
+    public static Difficulty fromId(int id) {
+        return switch (id) {
+            case 0 -> PEACEFUL;
+            case 1 -> EASY;
+            case 2 -> NORMAL;
+            case 3 -> HARD;
+            default -> throw new IllegalArgumentException(
+                    "Unknown difficulty id: " + id
+            );
+        };
+    }
 }
--- src/main/java/com/villy/minecraftmanager/enums/GameMode.java
+++ src/main/java/com/villy/minecraftmanager/enums/GameMode.java
@@ -1,7 +1,30 @@
 package com.villy.minecraftmanager.enums;
 
 public enum GameMode {
-    SURVIVAL,
-    CREATIVE,
-    HARDCORE
+    SURVIVAL(0),
+    CREATIVE(1),
+    ADVENTURE(2),
+    SPECTATOR(3);
+
+    private final int id;
+
+    GameMode(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static GameMode fromId(int id) {
+        return switch (id) {
+            case 0 -> SURVIVAL;
+            case 1 -> CREATIVE;
+            case 2 -> ADVENTURE;
+            case 3 -> SPECTATOR;
+            default -> throw new IllegalArgumentException(
+                    "Unknown game mode id: " + id
+            );
+        };
+    }
 }
